e6ae8835caa8289f3c203622beb8e68e74a2186e
Boanerge Joe <boanergejoe4@gmail.com>
2026-05-10 14:29:23 +0200

    update

--- MexicanRestaurant/obj/Debug/net10.0/MexicanRestaurant.AssemblyInfo.cs
+++ MexicanRestaurant/obj/Debug/net10.0/MexicanRestaurant.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MexicanRestaurant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b5ea409fdf2caa1d501fc21d6d9df3b66b88318")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38b92c5ba293c9d9034a0b0bcebbda975116f916")]
 [assembly: System.Reflection.AssemblyProductAttribute("MexicanRestaurant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MexicanRestaurant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
